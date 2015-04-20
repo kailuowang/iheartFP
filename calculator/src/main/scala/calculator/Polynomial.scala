cb931cfaf8e9022b7fa61f6a9f5678b65e861b1d
Kailuo Wang <kailuo.wang@gmail.com>
2015-04-20 15:53:46 -0400

    implemented the tweettext and polynomial

---
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,19 @@
+package calculator
+
+object Polynomial {
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double]): Signal[Double] = {
+    Signal {
+      val bb = b()
+      bb * bb - (4 * a() * c())
+    }
+  }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal {
+      val ds = Math.sqrt(delta())
+      Set((ds - b()) / (2* a()), (- ds - b()) / (2 * a()))
+    }
+  }
+}
